46ee736f602a3365808170413e0d1de06e6b7f2d
mutska <mutska@ciencias.unam.mx>
2018-10-19 22:46:50 -0500

    Interfaces para actualizar la base de datos incluida

--- reproductor.py
+++ reproductor.py
@@ -17,9 +17,8 @@ for record in cursor:
    tup = (record[4],record[9],record[13],record[7])
    lista.append(tup)
 cursor.close()
-
 db.close()
-#print(lista)
+
 
 
 def get_path(rel_path):
@@ -29,16 +28,21 @@ def get_path(rel_path):
     return abs_path_to_resource
 
 
-class DialogExample(Gtk.Dialog):
+class Edit_persons(Gtk.Dialog):
 
     def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "Edit Persons", parent, 0,
-                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-                             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        Gtk.Dialog.__init__(self, "Edit Persons", parent, 0)
         hbox = Gtk.HBox()
         hbox2 = Gtk.HBox()
         hbox3 = Gtk.HBox()
         hbox4 = Gtk.HBox()
+        hbox5 = Gtk.HBox()
+
+        button = Gtk.Button()
+        button.set_label("Save")
+        button2 = Gtk.Button()
+        button2.set_label("Cancel")
+
         self.set_default_size(300, 200)
         entry = Gtk.Entry()
         entry2 = Gtk.Entry()
@@ -56,24 +60,33 @@ class DialogExample(Gtk.Dialog):
         hbox3.pack_end(entry3, True, True, 0)
         hbox4.pack_start(label4, False, 2, 0)
         hbox4.pack_end(entry4, True, True, 0)
+        hbox5.pack_start(button, False, 2, 0)
+        hbox5.pack_end(button2, True, True, 0)
         box = self.get_content_area()
         box.set_spacing(5)
         box.add(hbox)
         box.add(hbox2)
         box.add(hbox3)
         box.add(hbox4)
+        box.add(hbox5)
         self.show_all()
 
 
-class DialogExample2(Gtk.Dialog):
+class Edit_groups(Gtk.Dialog):
 
     def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "Edit Group", parent, 0,
-                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-                             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        Gtk.Dialog.__init__(self, "Edit Group", parent, 0)
+
         hbox = Gtk.HBox()
         hbox2 = Gtk.HBox()
         hbox3 = Gtk.HBox()
+        hbox4 = Gtk.HBox()
+
+        button = Gtk.Button()
+        button.set_label("Save")
+        button2 = Gtk.Button()
+        button2.set_label("Cancel")
+
         self.set_default_size(300, 200)
         entry = Gtk.Entry()
         entry2 = Gtk.Entry()
@@ -87,15 +100,30 @@ class DialogExample2(Gtk.Dialog):
         hbox2.pack_end(entry2, True, True, 0)
         hbox3.pack_start(label3, False, 2, 0)
         hbox3.pack_end(entry3, True, True, 0)
+        hbox4.pack_start(button, False, 2, 0)
+        hbox4.pack_end(button2, True, True, 0)
         box = self.get_content_area()
         box.set_spacing(5)
         box.add(hbox)
         box.add(hbox2)
         box.add(hbox3)
+        box.add(hbox4)
+        button.connect("clicked", self.saving,entry, entry2, entry3)
         self.show_all()
+    def saving(self,widget,entry,entry2,entry3):
+        name = entry.get_text()
+        start_date = entry2.get_text()
+        end_date = entry3.get_text()
+        tup = (name,start_date,end_date)
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("insert into groups(name,start_date,end_date) values(?,?,?)",tup)
+        db.commit()
+        cursor.close()
+        db.close()
 
 
-class DialogWindow(Gtk.Window):
+class Edit_on_groups_and_persons(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Editions")
@@ -103,32 +131,242 @@ class DialogWindow(Gtk.Window):
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
         self.button1 = Gtk.Button(label="Edit Persons")
-        self.button1.connect("clicked", self.on_button_clicked)
+        self.button1.connect("clicked", self.on_button_persons)
         self.box.pack_start(self.button1, True, True, 0)
         self.button2 = Gtk.Button(label="Edit Groups")
-        self.button2.connect("clicked", self.on_button_clicked2)
+        self.button2.connect("clicked", self.on_button_groups)
         self.box.pack_start(self.button2, True, True, 0)
 
-    def on_button_clicked(self, widget):
-        dialog = DialogExample(self)
+    def on_button_persons(self, widget):
+        dialog = Edit_persons(self)
         response = dialog.run()
-
-        if response == Gtk.ResponseType.OK:
-            print("The OK button was clicked")
-        elif response == Gtk.ResponseType.CANCEL:
-            print("The Cancel button was clicked")
-
         dialog.destroy()
 
-    def on_button_clicked2(self, widget):
-        dialog = DialogExample2(self)
+    def on_button_groups(self, widget):
+        dialog = Edit_groups(self)
         response = dialog.run()
+        dialog.destroy()
 
-        if response == Gtk.ResponseType.OK:
-            print("The OK button was clicked")
-        elif response == Gtk.ResponseType.CANCEL:
-            print("The Cancel button was clicked")
+class Edit_song(Gtk.Dialog):
 
+    def __init__(self, parent,title,performer,album,genre):
+        Gtk.Dialog.__init__(self, "Edit Song", parent, 0)
+
+        button = Gtk.Button()
+        button.set_label("Save")
+        button2 = Gtk.Button()
+        button2.set_label("Cancel")
+
+        found = []
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("select rolas.track from rolas where title == ?",(title,)
+            )
+        for record in cursor:
+            found.append(record[0])
+
+        cursor.execute("select rolas.year from rolas where title == ?", (title,)
+                       )
+        for record in cursor:
+            found.append(record[0])
+        cursor.close()
+        db.close()
+        hbox = Gtk.HBox()
+        hbox2 = Gtk.HBox()
+        hbox3 = Gtk.HBox()
+        hbox4 = Gtk.HBox()
+        hbox5 = Gtk.HBox()
+        self.set_default_size(300, 200)
+        entry = Gtk.Entry()
+        entry.set_text(title)
+        entry2 = Gtk.Entry()
+        entry2.set_text(str(found[0]))
+        entry3 = Gtk.Entry()
+        entry3.set_text(str(found[1]))
+        entry4 = Gtk.Entry()
+        entry4.set_text(genre)
+        label = Gtk.Label("Title    : ")
+        label2 = Gtk.Label("Track:   ")
+        label3 = Gtk.Label("Year: ")
+        label4 = Gtk.Label("Genre: ")
+        hbox.pack_start(label, False, 2, 0)
+        hbox.pack_end(entry, True, True, 0)
+        hbox2.pack_start(label2, False, 2, 0)
+        hbox2.pack_end(entry2, True, True, 0)
+        hbox3.pack_start(label3, False, 2, 0)
+        hbox3.pack_end(entry3, True, True, 0)
+        hbox4.pack_start(label4, False, 2, 0)
+        hbox4.pack_end(entry4, True, True, 0)
+        hbox5.pack_start(button, True, True, 0)
+        hbox5.pack_end(button2, True, True, 0)
+        box = self.get_content_area()
+        box.set_spacing(5)
+        box.add(hbox)
+        box.add(hbox2)
+        box.add(hbox3)
+        box.add(hbox4)
+        box.add(hbox5)
+        button.connect("clicked", self.saving,title,entry,entry2,entry3,entry4)
+        self.show_all()
+    def saving(self,widget,original_title,entry,entry2,entry3,entry4):
+        title = entry.get_text()
+        track = entry2.get_text()
+        year = entry3.get_text()
+        genre = entry4.get_text()
+        tup = (title,track,year,genre,original_title)
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("update rolas set title = ?,track = ?,year = ?,genre = ? where rolas.title == ?",tup)
+        db.commit()
+        cursor.close()
+        db.close()
+
+class Edit_album(Gtk.Dialog):
+
+    def __init__(self, parent,album):
+        Gtk.Dialog.__init__(self, "Edit album", parent, 0)
+
+        hbox = Gtk.HBox()
+        hbox2 = Gtk.HBox()
+        hbox3 = Gtk.HBox()
+        self.set_default_size(300, 200)
+        button = Gtk.Button()
+        button.set_label("Save")
+        button2 = Gtk.Button()
+        button2.set_label("Cancel")
+        found = []
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("select albums.name from albums where name == ?", (album,)
+                       )
+        for record in cursor:
+            found.append(record[0])
+
+        cursor.execute("select albums.year from albums where name == ?", (album,)
+                       )
+        for record in cursor:
+            found.append(record[0])
+        cursor.close()
+        db.close()
+
+        entry = Gtk.Entry()
+        entry.set_text(album)
+        entry2 = Gtk.Entry()
+        entry2.set_text(str(found[1]))
+        label = Gtk.Label("Name: ")
+        label2 = Gtk.Label("Year:   ")
+        hbox.pack_start(label, False, 2, 0)
+        hbox.pack_end(entry, True, True, 0)
+        hbox2.pack_start(label2, False, 2, 0)
+        hbox2.pack_end(entry2, True, True, 0)
+        hbox3.pack_start(button, False, 2, 0)
+        hbox3.pack_end(button2, True, True, 0)
+        box = self.get_content_area()
+        box.set_spacing(5)
+        box.add(hbox)
+        box.add(hbox2)
+        box.add(hbox3)
+        button.connect("clicked", self.saving, album, entry, entry2)
+        self.show_all()
+    def saving(self, widget, original_title, entry, entry2):
+        name = entry.get_text()
+        year = entry2.get_text()
+        tup = (name,  year, original_title)
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("update albums set name = ?,year = ? where albums.name == ?", tup)
+        db.commit()
+        cursor.close()
+        db.close()
+class Edit_performer(Gtk.Dialog):
+
+    def __init__(self, parent,performer):
+        Gtk.Dialog.__init__(self, "Edit performer", parent, 0)
+
+        hbox = Gtk.HBox()
+        hbox2 = Gtk.HBox()
+        hbox3 = Gtk.HBox()
+
+        self.set_default_size(300, 200)
+        button = Gtk.Button()
+        button.set_label("Save")
+        button2 = Gtk.Button()
+        button2.set_label("Cancel")
+        found = []
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("select performers.name from performers where name == ?", (performer,)
+                       )
+        for record in cursor:
+            found.append(record[0])
+
+        cursor.execute("select performers.id_type from performers where name == ?", (performer,)
+                       )
+        for record in cursor:
+            found.append(record[0])
+        cursor.close()
+        db.close()
+
+        entry = Gtk.Entry()
+        entry.set_text(performer)
+        entry2 = Gtk.Entry()
+        entry2.set_text(str(found[1]))
+        label = Gtk.Label("Name: ")
+        label2 = Gtk.Label("Type(0/1/2):   ")
+        hbox.pack_start(label, False, 2, 0)
+        hbox.pack_end(entry, True, True, 0)
+        hbox2.pack_start(label2, False, 2, 0)
+        hbox2.pack_end(entry2, True, True, 0)
+        hbox3.pack_start(button, False, 2, 0)
+        hbox3.pack_end(button2, True, True, 0)
+        box = self.get_content_area()
+        box.set_spacing(5)
+        box.add(hbox)
+        box.add(hbox2)
+        box.add(hbox3)
+        button.connect("clicked", self.saving, performer, entry, entry2)
+        self.show_all()
+
+    def saving(self, widget, performer, entry, entry2):
+        name = entry.get_text()
+        type = entry2.get_text()
+        tup = (name, type, performer)
+        db = sqlite3.connect("Reproductor.sqlite")
+        cursor = db.cursor()
+        cursor.execute("update performers set name = ?,id_type = ? where performers.name == ?", tup)
+        db.commit()
+        cursor.close()
+        db.close()
+
+class Songs(Gtk.Window):
+
+    def __init__(self,title,performer,album,genre):
+        Gtk.Window.__init__(self, title="Edit song properties")
+        self .set_border_width(6)
+        self.box = Gtk.Box(spacing=6)
+        self.add(self.box)
+        self.button1 = Gtk.Button(label="Edit Song")
+        self.button1.connect("clicked", self.on_button_song,title,performer,album,genre)
+        self.box.pack_start(self.button1, True, True, 0)
+        self.button2 = Gtk.Button(label="Edit Album")
+        self.button2.connect("clicked", self.on_button_album,album)
+        self.box.pack_start(self.button2, True, True, 0)
+        self.button3 = Gtk.Button(label="Edit Performer")
+        self.button3.connect("clicked", self.on_button_performer,performer)
+        self.box.pack_start(self.button3, True, True, 0)
+
+    def on_button_song(self, widget,title,performer,album,genre):
+        dialog = Edit_song(self,title,performer,album,genre)
+        response = dialog.run()
+        dialog.destroy()
+
+    def on_button_album(self, widget,album):
+        dialog = Edit_album(self,album)
+        response = dialog.run()
+        dialog.destroy()
+    def on_button_performer(self, widget,performer):
+        dialog = Edit_performer(self,performer)
+        response = dialog.run()
         dialog.destroy()
 
 
@@ -155,8 +393,7 @@ class TreeViewFilterWindow(Gtk.Window):
 
         #Creacion del  filter con el "liststore model"
         self.search_filter = self.fields.filter_new()
-        #setting the filter function, note that we're not using the
-        #self.search_filter.set_visible_func(self.search_filter_func)
+
 
         #Creacion del  treeview, usando el  filter como  model.
         self.treeview = Gtk.TreeView.new_with_model(self.search_filter)
@@ -186,9 +423,9 @@ class TreeViewFilterWindow(Gtk.Window):
                 self.img1 = Gtk.Image()
                 self.img1.set_from_icon_name("avatar-default", 1)
                 button.set_image(self.img1)
+                button.connect("clicked", self.on_enter)
 
             button.set_image_position(Gtk.PositionType.RIGHT)
-            button.connect("clicked", self.on_entry_enter)
 
         #Configuracion de un entry
         self.entry = Gtk.Entry()
@@ -212,9 +449,11 @@ class TreeViewFilterWindow(Gtk.Window):
     def on_db_click(self, tree_view,path,column):
             model = tree_view.get_model()
             iter = model.get_iter(path)
-            print(model[iter][0])
-            win = DialogWindow()
-            #win.connect("destroy", Gtk.main_quit)
+            title = model[iter][0]
+            performer = model[iter][1]
+            album = model[iter][2]
+            genre = model[iter][3]
+            win = Songs(title,performer,album,genre)
             win.show_all()
 
     def on_row_click(self,widget):
@@ -251,20 +490,18 @@ class TreeViewFilterWindow(Gtk.Window):
         cursor.close()
         db.close()
         print("Busqueda de patrón : %s" % temp)
-        # setting the filter function, note that we're not using the
+        # Configuramos la funcion de filtrado
         self.search_filter.set_visible_func(self.search_filter_func)
         # Actualizamos "filter" para que actualize la vista
         self.search_filter.refilter()
 
     def on_enter(self,widget):
-        text = widget.get_text()
-        print(text)
-        return text
+        win = Edit_on_groups_and_persons()
+        win.show_all()
 
 
 
 win = TreeViewFilterWindow()
-#print(dir(win.props))
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
